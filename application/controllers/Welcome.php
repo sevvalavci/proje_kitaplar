55519b4f45b26dcef58ffe1a0994eaaf58071e6c
hanificanseven <hanifi.anelka@hotmail.com>
2022-04-23 18:45:37 +0300

    index değişti bootstrap kuruldu

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,10 +6,7 @@ class Welcome extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('proje.php');
 	}
-	public function welcome()
-	{
-		echo "Hoşgeldiniz";
-	}
+	
 }
