57ed4553c4e603854cb8cbf5b06c4b274ea3db43
hanificanseven <hanifi.anelka@hotmail.com>
2022-04-27 18:46:54 +0300

    footer değişiklikleri

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:85/proje_kitaplar/';
+$config['base_url'] = 'http://localhost/proje_kitaplar/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -5,37 +5,41 @@
     <div class="footer bg-footer section border-bottom">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-sm-8 mb-5 mb-lg-0">
-                    <!-- logo -->
-                    <a class="logo-footer" href="index.html"><img class="img-fluid mb-4"
-                            src="<?php echo base_url("assets/"); ?>images/logo.png" alt="logo"></a>
-                    
-                </div>
+
                 <!-- company -->
-                <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
-                    <h4 class="text-white mb-5">Biz Kimiz?</h4>
-                    <ul class="list-unstyled">
-                        <li class="mb-3"><a class="text-color" href="#hakkimizda">Hakkımızda</a></li>
-                       
-                    </ul>
-                </div>
+
                 <!-- links -->
-                <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
-                    <h4 class="text-white mb-5">Satıslarımız</h4>
-                    <ul class="list-unstyled">
-                        <li class="mb-3"><a class="text-color" href="<?php echo base_url("welcome/all_books"); ?>">Kitaplar</a></li>
-                       
-                    </ul>
+                <div class="col-md-6">
+                    <h4 class="text-white mb-5">Kitap okurken dinleyebiliceğiniz şarkılar</h4>
+
+                    <li class="mb-0"><a class="text-color">
+                            <p>kitap okumak, konsantrasyon gerektiren bir iştir. Bu
+                                işi yaparken müzik dinlemek çoğu zaman dikkatinizi dağıtabilir. Yine de duygusal
+                                durumu
+                                olumlu etkileyen bu süreçlerle ilgili dilediğiniz müzik tarzına ulaşmak için Mp3
+                                indir
+                                seçeneklerinden faydalanabilirsiniz. Müzik her ne kadar ruhsal gelişime katkıda
+                                bulunsa
+                                da konsantrasyon gerektiren işlerde uzak durulması gereken bir aktivite olduğu
+                                unutulmamalıdır.</p>
+                    </li>
+
+
                 </div>
                 <!-- support -->
-                <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
-                    <h4 class="text-white mb-5">BİZE ULASIN</h4>
-                    <ul class="list-unstyled">
-                        <li class="mb-3"><a class="text-color" href="<?php echo base_url("welcome/contact"); ?>">İLETİSİM</a></li>
-                        
-                    </ul>
+                <div class="col-md-5">
+
                 </div>
                 <!-- support -->
+                <div class="col-md-50">
+
+                    <iframe style="border-radius:12px"
+                        src="https://open.spotify.com/embed/playlist/4QiDLwqIL3Em5WS10TkyHg?utm_source=generator&theme=0"
+                        width="100%" height="380" frameBorder="0" allowfullscreen=""
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+
+
+                </div>
 
             </div>
         </div>
