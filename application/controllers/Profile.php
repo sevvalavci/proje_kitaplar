f7e9c0c8e42202581c2870753882f410d99b500f
Sevval <sevval@adu.edu.tr>
2022-04-28 16:57:28 +0300

    profil yapılmaya başlandı

---
+++ application/controllers/Profile.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Profile extends CI_Controller
+
+{   public function index(){
+    $this->load->view('profile_view');
+
+}
+   
+}
+
+?>
