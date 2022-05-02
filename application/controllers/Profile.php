9c1f0d739c43e774f943a007c769842d715c9c72
hanificanseven <hanifi.anelka@hotmail.com>
2022-05-02 16:01:08 +0300

    s

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -1,23 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-<<<<<<< HEAD
-class profile extends CI_Controller
+
+class Profile extends CI_Controller
 {
     public function index()
     {
         $this->load->view('includes/profil.php');
     }
 }
-=======
-class Profile extends CI_Controller
-
-{   public function index(){
-    $this->load->view('profile_view');
-
-}
-   
-}
-
-?>
->>>>>>> f7e9c0c8e42202581c2870753882f410d99b500f
