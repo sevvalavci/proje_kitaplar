17f1ab07c4267854957abbc26084dbf49d7e9cd9
Sevval <sevval@adu.edu.tr>
2022-05-06 01:32:31 +0300

    profilim

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -6,6 +6,49 @@ class Profile extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('includes/profil.php');
+        $this->load->view('includes/profil');
+    }
+    
+    public function profiledata()
+    {
+        if($this->input->method() == "post"){
+            $this->$this->form_validation->set_rules('kadi', 'Kullanıcı adı', 'trim|required');
+            $this->$this->form_validation->set_rules('eposta', 'E-posta adresi', 'trim|required');
+            
+            if($this->$this->form_validation->run() == FALSE){
+                echo validation_errors();
+
+            }
+            else
+            {
+                $query =$this->common_model->get([
+                'eposta'=>$this->input->post('eposta',true),
+                'id !='=>$this->session->userdata('id')], 'uyeler');
+                  
+                 if($query){
+                     echo "E-posta adresi sistemde kayıtlı";
+                 
+                 }
+                 else
+                 {
+                     $data = array(
+                         "kadi" => strip_tags(trim($this->input->post('kadi',true))),
+                         "eposta" => strip_tags(trim($this->input->post('eposta',true)))
+
+                     );
+                 }
+
+                 $update = $this->common_model->update(['id'=>$this->session->userdata('id')],$data,'uyeler');
+                 if($update){
+                     echo "Profiliniz güncellendi";
+
+                 }
+                 else{
+                     echo "Hata oluştu.";
+                 }
+
+            }
+        }
     }
 }
+?>
